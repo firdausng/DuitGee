8f177d581c1227fe306adc7c117d52e6ea78e62f
Firdaus Kamaruddin <mail4frenz@gmail.com>
2025-12-11 22:54:44 +0800

    Initialize PostHog tracking in production environments
    - Add `onMount` lifecycle method to initialize PostHog with environment-specific configurations.
    - Ensure client-side tracking is only set up in browser environments outside of development mode.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,24 @@
 	import posthog from 'posthog-js';
 	import favicon from '$lib/assets/favicon.svg';
     import { ModeWatcher } from "mode-watcher";
+	import {onMount} from "svelte";
+	import {browser, dev} from "$app/environment";
 	
-	let { children } = $props();
+	let { children, data } = $props();
+
+	onMount(() => {
+		if (browser && !dev) {
+			posthog.init(
+					data.posthogKey,
+					{
+						api_host: 'https://us.i.posthog.com',
+						defaults: '2025-05-24',
+						person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
+					}
+			)
+		}
+		return
+	});
 </script>
 
 <svelte:head>
